9b29c68a6d6cc4999f2101eac9fc3715d68a7f31
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-11-21 20:50:53 +0600

    added input validation custom error message

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// function formValidation() {
+//     const inputEle = document.getElementById('id1');
+
+//     if(inputEle.checkValidity()) {
+//         validationMessage;
+//     }
+// }
+
+
 function formValidation() {
     const inputEle = document.getElementById('id1');
-
-    if(inputEle.checkValidity()) {
-        inputEle.validationMessage;
-    }
+    
+    if(inputEle.validity.rangeOverflow) {
+        inputEle.setCustomValidity("You have made a range overflow error!");
+    }else if(inputEle.validity.rangeUnderflow) {
+        inputEle.setCustomValidity("You have made a range underflow error!");
+    }else if(inputEle.validity.valueMissing) {
+        inputEle.setCustomValidity("Value Missing error!");
+    }else{
+        validationMessage;
+    }  
 }
